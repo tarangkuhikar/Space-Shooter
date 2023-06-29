417c1a5bbfc4fb89b05f6e9caab77743ccb93b36
tarangkuhikar <kuhikartarang@gmail.com>
2023-06-29 12:43:03 +0530

    failed commit

--- Assets/Scripts/PlayerScripts/SpaceshipController.cs
+++ Assets/Scripts/PlayerScripts/SpaceshipController.cs
@@ -10,17 +10,21 @@ public class SpaceshipController : MonoBehaviour
 
     Rigidbody2D _spaceShip;
     //Rect cameraRect;
-
+   
     Vector3 _screenBounds;
     Vector3 viewPos;
     float _objectWidth, _objectHeight;
+    bool _outOfBounds=false;
+    [SerializeField]
+    GameObject _secondSpaceShip;
     void OnEnable()
     {
+       
         _screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
         _objectWidth = transform.GetComponent<SpriteRenderer>().bounds.extents.x;
         _objectHeight = transform.GetComponent<SpriteRenderer>().bounds.extents.y;
         _spaceShip = GetComponent<Rigidbody2D>();
-
+        
     }
 
     void FixedUpdate()
@@ -30,9 +34,15 @@ public class SpaceshipController : MonoBehaviour
 
     private void LateUpdate()
     {
-        viewPos = transform.position;
-        viewPos.x = Mathf.Clamp(viewPos.x, _screenBounds.x * -1 + _objectWidth, _screenBounds.x - _objectWidth);
-        viewPos.y = Mathf.Clamp(viewPos.y, _screenBounds.y * -1 + _objectHeight, _screenBounds.y - _objectHeight);
-        transform.position = viewPos;
+        /*   viewPos = transform.position;
+           viewPos.x = Mathf.Clamp(viewPos.x, _screenBounds.x * -1 + _objectWidth, _screenBounds.x - _objectWidth);
+           viewPos.y = Mathf.Clamp(viewPos.y, _screenBounds.y * -1 + _objectHeight, _screenBounds.y - _objectHeight);
+           transform.position = viewPos;*/
+       
+        if (transform.position.x >= _screenBounds.x-_objectWidth && _outOfBounds==false)
+        {
+            Instantiate(_secondSpaceShip,new Vector3(gameObject.transform.position.x-2*_screenBounds.x-_objectWidth,gameObject.transform.position.y), Quaternion.Euler(0,0,180));
+            _outOfBounds = true;   
+        }
     }
 }
