6a7ecea0e007be86ef1f663de844e754f3cad353
tarangkuhikar <tarangkuhikar@gmail.com>
2023-07-07 15:50:02 +0530

    Revert "trying to improve gamemanager and gamespawner,failed"
    This reverts commit b07f2a332e762ddfff4c1664273c70d5677e65ae.

--- Assets/Scripts/EnemyScripts/EnemySpawner.cs
+++ Assets/Scripts/EnemyScripts/EnemySpawner.cs
@@ -9,10 +9,8 @@ public class EnemySpawner : MonoBehaviour
 
     [SerializeField]
     float _delayBetweenEnemies;
-
     [SerializeField]
     float _delayBetweenWaves;
-
     [SerializeField]
     Grid _grid;
 
@@ -20,20 +18,28 @@ public class EnemySpawner : MonoBehaviour
     Vector3[] _spawnPoints;
 
     [SerializeField]
-    EnemyBehaviour _enemyPrefab;
+    EnemyBehaviour[] _enemyPrefab;
 
-    public static List<EnemyBehaviour> _enemyList;
+    static int _enemyActive = 0;
+
+    List<EnemyBehaviour> _enemyList;
+
+    List<Vector3> PathPoints;
 
     [SerializeField]
-    private float _fireChance;
+    float fireChance = 0.3f;
     [SerializeField]
-    private float _delayBetweenDives;
+    float _delayBetweenDives = 5f;
+    int _enemyType1Spawned = 0;
 
     public void Start()
     {
+        _enemyActive = 0;
+        EnemyBehaviour.EnemyKilled += EnemyKilled;
         PlayerBehaviour.PlayerDied += PlayerBehaviour_PlayerDied;
         _enemyList = new List<EnemyBehaviour>();
-        StartCoroutine(SpawnEnemies());
+        PathPoints = new List<Vector3>();
+        StartCoroutine(SpawnEnemies2());
     }
 
     private void PlayerBehaviour_PlayerDied()
@@ -42,35 +48,118 @@ public class EnemySpawner : MonoBehaviour
     }
     IEnumerator SpawnEnemies()
     {
+        _enemyType1Spawned += 1;
         for (int j = 0; j < _spawnPoints.Length; j++)
         {
             for (int i = 0; i < _waveSize; i++)
             {
-                _enemyList.Add(Instantiate(_enemyPrefab, _spawnPoints[j], Quaternion.identity));
-                _enemyList[j * _waveSize + i].MoveGridPos(_grid.CellToWorld(new Vector3Int(-2 * i + _waveSize + j % 2, -j, 0)));
+
+                _enemyList.Add(Instantiate(_enemyPrefab[0], _spawnPoints[j], Quaternion.identity));
+                _enemyActive += 1;
+                PathPoints.Add(_grid.CellToWorld(new Vector3Int(-2 * i + _waveSize + j % 2, -j, 0)));
+
+                _enemyList[_waveSize * j + i].SetPath(PathPoints.ToArray(), 2);
+
+                PathPoints.Clear();
                 yield return new WaitForSeconds(_delayBetweenEnemies);
             }
             yield return new WaitForSeconds(_delayBetweenWaves);
         }
+        yield return new WaitForSeconds(3);
         StartCoroutine(DiveEnemies());
     }
 
+
     IEnumerator DiveEnemies()
     {
-        for (int i = 0; i < _waveSize; i++)
+        while (true)
         {
-            int x = Random.Range(0, _enemyList.Count - 1);
-            
-            _enemyList[x].DiveTowardsPlayer();
-            if (Random.value < _fireChance)
+            int j = Random.Range(0, _spawnPoints.Length);
+            Vector3 x = GameManager.Player.position - Random.Range(-1.5f, 1.5f) * Vector3.right;
+            for (int i = 0; i < _waveSize; i++)
             {
-                _enemyList[x].Invoke("FirePattern", Random.Range(0.5f,2f));
+                if (_enemyList[j * _waveSize + i] != null)
+                {
+                    _enemyList[j * _waveSize + i].SetPath(new Vector3[] { x - 2 * Vector3.down - 1 * Vector3.right, x - 2 * Vector3.down - 1 * Vector3.left, _grid.CellToWorld(new Vector3Int(-2 * i + _waveSize + j % 2, -j, 0)) }, 2);
+                    yield return new WaitForSeconds(0.2f);
+                    if (Random.value < fireChance)
+                    {
+                        StartCoroutine(FireBulletsRandomly(j * _waveSize + i));
+                    }
+                }
             }
             yield return new WaitForSeconds(_delayBetweenDives);
+
         }
     }
+
+    IEnumerator FireBulletsRandomly(int EnemyIndex)
+    {
+        yield return new WaitForSeconds(Random.Range(1f, 3f));
+        if (_enemyList[EnemyIndex] != null)
+        {
+            _enemyList[EnemyIndex].FirePattern();
+        }
+    }
+
+    IEnumerator SpawnEnemies2()
+    {
+        float x = Random.Range(-1f, 1f);
+        int y = Random.Range(0, 15);
+        yield return new WaitForSeconds(3);
+        for (int i = 0; i < _waveSize; i++)
+        {
+            _enemyList.Add(Instantiate(_enemyPrefab[1], _spawnPoints[1] + 3 * Vector3.up, Quaternion.identity));
+            _enemyActive += 1;
+            _enemyList[i].SetPath(new Vector3[] { _spawnPoints[1] + 3 * Vector3.up, _spawnPoints[1] + y * Vector3.right + (x + 3) * Vector3.up, _spawnPoints[1] + (y + 3 * Mathf.Abs(x)) * Vector3.right - (x - 3) * Vector3.up, 10 * Vector3.right }, 7);
+            yield return new WaitForSeconds(0.1f);
+            StartCoroutine(FireBulletsRandomly(i));
+            yield return new WaitForSeconds(7 * _delayBetweenEnemies);
+        }
+
+        yield return new WaitForSeconds(8);
+        for (int i = 0; i < _waveSize; i++)
+        {
+
+            if (_enemyList[i] != null)
+            {
+
+                Destroy(_enemyList[i].gameObject);
+                EnemyKilled();
+            }
+        }
+    }
+    void EnemyKilled()
+    {
+
+        _enemyActive -= 1;
+
+        if (_enemyActive == 0)
+        {
+            fireChance += 0.2f;
+            if (_delayBetweenDives >= 1)
+            {
+                _delayBetweenDives -= 0.5f;
+            }
+
+            StopAllCoroutines();
+
+            _enemyList.Clear();
+            if (_enemyType1Spawned == 2)
+            {
+                StartCoroutine(SpawnEnemies2());
+                _enemyType1Spawned = 0;
+            }
+            else
+            {
+                StartCoroutine(SpawnEnemies());
+            }
+        }
+    }
+
     private void OnDestroy()
     {
+        EnemyBehaviour.EnemyKilled -= EnemyKilled;
         PlayerBehaviour.PlayerDied -= PlayerBehaviour_PlayerDied;
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -27,14 +27,45 @@ public class GameManager : MonoBehaviour
     {
         _explosionAudio = gameObject.GetComponent<AudioSource>();
         Player = _player;
-
         PlayerBehaviour.PlayerDied += PlayerBehaviour_PlayerDied;
 
+        _helperText.text = "";
         yield return new WaitForSeconds(1);
 
+        yield return StartCoroutine(ShowHelperText("Use the A,D or left arrow and right arrow key to move."));
+
+        yield return new WaitUntil(() => Input.GetButtonDown("Horizontal"));
+        
+        yield return new WaitForSeconds(0.5f);
+        _helperText.text = "";
+
+
+        yield return StartCoroutine(ShowHelperText("Use the F or Space key to shoot."));
+        yield return new WaitUntil(() => Input.GetButtonDown("Fire1"));
+
+
+        yield return new WaitForSeconds(0.5f);
+        _helperText.text = "";
+        yield return new WaitForSeconds(1);
         _enemySpawner.gameObject.SetActive(true);
         yield return new WaitForSeconds(2);
         StartCoroutine(SpawnAsteroids());
+    }
+
+
+
+    IEnumerator ShowHelperText(string TextToShow)
+    {
+
+        _helperText.maxVisibleCharacters = 0;
+        int length = TextToShow.Length;
+        _helperText.text = TextToShow;
+        for (int i = 0; i < length; i++)
+        {
+            _helperText.maxVisibleCharacters += 1;
+            yield return new WaitForSeconds(0.02f);
+        }
+
     }
 
 
@@ -55,7 +86,6 @@ public class GameManager : MonoBehaviour
     {
         _explosionAudio.Play();
     }
-
     private void PlayerBehaviour_PlayerDied()
     {
         StopAllCoroutines();
